469345c5f643831c2118fbf198438d98ac4f90d6
Esther Mesa <estmesher@gmail.com>
2024-03-04 15:55:58 +0000

    RELATIONSHIPS - Relation models created - Relationships created - Syncronized

---
+++ api/models/user_event.model.js
@@ -0,0 +1,26 @@
+const { DataTypes } = require('sequelize')
+const sequelize = require('../../db')
+
+const User_Event = sequelize.define(
+    'user_event',
+    {
+        status: {
+            type: DataTypes.STRING,
+            defaultValue: "Accepted",
+            allowNull: false
+        },
+       rating: {
+            type: DataTypes.INTEGER,
+            validate: {
+                min: 1,
+                max: 5
+            }
+        },
+       comment: {
+            type: DataTypes.STRING
+        }
+    },
+    { timestamps: false }
+)
+
+module.exports = User_Event
---
+++ db/relationship.js
@@ -0,0 +1,15 @@
+const CategoryModel = require('../api/models/category.model')
+const EventModel = require('../api/models/event.model')
+const Event_Category = require('../api/models/event_category.model')
+const UserModel = require('../api/models/user.model')
+const User_Event = require('../api/models/user_event.model')
+
+const createRelationships = () => {
+    UserModel.belongsToMany(EventModel, { through: User_Event })
+    EventModel.belongsToMany(UserModel, { through: User_Event })
+
+    EventModel.belongsToMany(CategoryModel, { through: Event_Category })
+    CategoryModel.belongsToMany(EventModel, { through: Event_Category })
+}
+
+module.exports = createRelationships
