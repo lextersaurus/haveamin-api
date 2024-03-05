4563bd2dd70eed828fca56bbd8b3c031a6160e64
Scherezade Santana <sherezade1988@gmail.com>
2024-03-05 14:05:11 +0000

    Event: - Join and quit event

--- api/routes/event.routes.js
+++ api/routes/event.routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router()
-const { getEvents, getOneEvent,createEvent, updateEvent, deleteEvent } = require('../controllers/event.controller')
+const { getEvents, getOneEvent,createEvent, updateEvent, deleteEvent, joinEvent, quitEvent } = require('../controllers/event.controller')
 
 
 router.get('/showall', getEvents)
@@ -7,6 +7,8 @@ router.get('/show/:id', getOneEvent)
 router.post('/create', createEvent)
 router.put('/update/:id', updateEvent)
 router.delete('/delete/:id', deleteEvent)
+router.put('/:id/join', joinEvent)
+router.delete('/:id/quit', quitEvent)
 
 
 
