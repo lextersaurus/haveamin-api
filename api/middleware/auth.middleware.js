0cdf6fd5279b796f19681d5f19e9343104180d33
Fabio Herrera <fabio_hd_8@hotmail.com>
2024-03-05 10:02:10 +0000

    auth created checkauth and checkadmin middlewares

---
+++ api/middleware/auth.middleware.js
@@ -0,0 +1,43 @@
+const UserModel = require('../models/user.model')
+const jwt = require('jsonwebtoken')
+
+
+const checkAuth = (req, res, next) => {
+    if (!req.headers.authorization)
+        return res.status(401).send('Unauthorized')
+    jwt.verify(
+        req.headers.authorization,
+        process.env.JWT_SECRET, async (err, payload) => {
+
+            try {
+                if (err) return res.status(401).send('Unauthorized')
+                
+                const user = await UserModel.findOne({
+                    where: { 
+                        email: payload.email 
+                    }
+
+                })
+                if (!user) return res.status(401).send('Unauthorized')
+                
+                res.locals.user = user
+                next()
+                
+            } catch (error) {
+                console.log(error)
+                res.status(500).send('Unauthorized')
+                
+            }
+           
+        })
+
+}
+const checkAdmin = (req, res, next)=>{
+    if (res.locals.user.role !== 'admin') return res.status(401).send('User not authorized')
+    
+        next()
+    
+}
+
+
+module.exports = {checkAuth, checkAdmin}
--- api/routes/index.js
+++ api/routes/index.js
@@ -3,10 +3,11 @@ const AuthRouter = require('./auth.routes')
 const EventRouter = require('./event.routes')
 const CategoryRouter = require('./category.routes')
 const UserRouter = require('./user.routes')
+const {checkAuth} = require('../middleware/auth.middleware')
 
 router.use('/auth', AuthRouter)
-router.use('/category', CategoryRouter)
-router.use('/event', EventRouter)
-router.use('/user', UserRouter)
+router.use('/category', checkAuth, CategoryRouter)
+router.use('/event', checkAuth, EventRouter)
+router.use('/user', checkAuth, UserRouter)
 
 module.exports = router
